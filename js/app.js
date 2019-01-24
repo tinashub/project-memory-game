08c99afc3c29c88fc4aef41561ce046264aed5f7
tinashub <tina4purpose@gmail.com>
2019-01-24 20:39:16 +0100

    memory game phase 2
    Optimized functions, improved click target detecting, added reset
    listener and functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+/*
+ * Create a list that holds all of your cards
+ */
+
+const orderedCards = ["fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-leaf", "fa-diamond", "fa-anchor", "fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-leaf", "fa-diamond", "fa-anchor"];
+let openCards = [];
+let matchedPairs = 0;
+let playedTurns = 0;
+paintCards();
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+ 
+function paintCards() {
+	let randomCards = shuffle(orderedCards);
+	let htmlString = "";
+	for (let i = 0; i < 16; i++) {
+		htmlString += '<li class="card"><i class="fa '+randomCards[i]+'"></i></li>';
+	}
+	document.querySelector('.deck').innerHTML = htmlString;
+	openCards = [];
+	matchedPairs = 0;
+	playedTurns = 0;
+	document.querySelector('.moves').innerText = 0;
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -13,26 +44,6 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * Create a list that holds all of your cards
- */
-
-const orderedCards = ["fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-leaf", "fa-diamond", "fa-anchor", "fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-leaf", "fa-diamond", "fa-anchor"];
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
- let randomCards = shuffle(orderedCards);
- let openCards = [];
- let htmlString = "";
- for (let i = 0; i < 16; i++) {
-    htmlString += '<li class="card"><i class="fa '+randomCards[i]+'"></i></li>';
-}
- document.querySelector('.deck').innerHTML = htmlString;
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -45,25 +56,47 @@ const orderedCards = ["fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "f
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- document.querySelector('.deck').addEventListener('click', respondToTheClick);
+ document.querySelector('.deck').addEventListener('click', function (evt) {
+	var y = evt.target;
+	if (y.nodeName === 'I') {
+		y = y.parentElement;
+	}
+	if ((y.nodeName === 'LI')&&(y.className === 'card')) {  
+		showCard(y);
+		openCards.push(y);
+		if (openCards.length == 2) {
+			compareCards();
+		}
+	}
+});
+ 
+ document.querySelector('.restart').addEventListener('click', paintCards);
  
  function showCard(card) {
 	card.setAttribute('class', 'card open show');
  }
  
  function hideCard(card) {
-	setTimeout(card.setAttribute('class', 'card'), 3000);
+	card.setAttribute('class', 'card');
  }
  
  function matchCard(card) {
 	card.setAttribute('class', 'card match');
  }
  
- function compareCards(c1,c2) {
+ function compareCards() {
+	playedTurns++;
+	document.querySelector('.moves').innerText = playedTurns;
+	let c1 = openCards.pop();
+	let c2 = openCards.pop();
 	setTimeout(function () {
 		if (c1.innerHTML == c2.innerHTML) {
 			matchCard(c1);
 			matchCard(c2);
+			matchedPairs++;
+			if (matchedPairs == 8) {
+				showWin();
+			}
 		}
 		else {
 			hideCard(c1);
@@ -72,18 +105,8 @@ const orderedCards = ["fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "f
     }, 500);
 }
 	 
+ function showWin() {
+	 //addStar();
+	 alert("DONE");
+ }
 	 
- function respondToTheClick(evt) {
-	var y = evt.target;
-	if ((y.nodeName === 'LI')&&(y.className === 'card')) {  
-		showCard(y);
-		console.log(y.className);
-		if (openCards.length > 0) {
-			var x = openCards.pop();
-			compareCards(x,y);
-		}
-		else {
-			openCards.push(evt.target); 
-		}
-	}
-}
