8aa5a907921e9f5e368339d5cbdad8b5f18aeb48
tinashub <tina4purpose@gmail.com>
2019-08-08 15:53:19 +0200

    final version

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# project-memory-game
+# Memory Game Project
+
+## Table of Contents
+
+* [Description](#description)
+* [Instructions](#instructions)
+* [Dependencies](#dependencies)
+* [Contributing](#contributing)
+
+## Description
+
+Classic Memory Game. The user flips 2 consecutive cards over. If they match, they stay visible, if not, they hide again.
+
+User wins when all cards get visible. Functionality also includes restart, score, timer and winning message.
+
+## Instructions
+
+Please download the complete game including index.html file and folders css, img and js.
+
+To start the game, double-click index.html.
+
+Click on 2 cards on the board consecutively. The card faces are shown, then both cards stay visible if they match. When they don't match, both card are hidden again.
+
+The upper panel shows the number of moves and how long you are playing. The number of stars reflects your success in finding matching cards. 
+
+Click on the restart button makes all your scores reset, and you start again with the new board.
+
+You win the game when all the cards get visible. The winning message informs you on your final score.
+
+## Dependencies
+
+Along with run file index.html, the app uses app.css file located in css folder, app.js file in js folder and geometry2.png file in img folder.
+
+The app also needs access to https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css and https://fonts.googleapis.com/css?family=Coda when running, for correct styling.
+
+## Contributing
+
+Starter code provided by Udacity.
+
+Final version developed by Valentina Perfilov.
--- js/app.js
+++ js/app.js
@@ -1,12 +1,43 @@
 /*
  * Create a list that holds all of your cards
  */
-
 const orderedCards = ["fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-leaf", "fa-diamond", "fa-anchor", "fa-cube", "fa-paper-plane-o", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-leaf", "fa-diamond", "fa-anchor"];
+
+/*
+ * Set initial values
+ */
 let openCards = [];
 let matchedPairs = 0;
 let playedTurns = 0;
+let gainedStars = 3;
+let	timerId = null;
+let playedSeconds = 0;
 paintCards();
+document.querySelector('.win-cont').style.display = "none";
+
+/*
+ * Set up the event listener for a card. If a card is clicked:
+ * check what node is clicked, correct the target
+ * display the card's symbol if it is not already displayed
+ * add the card to a *list* of "open" cards
+ * if the list already has another card, check to see if the two cards match
+ */
+document.querySelector('.deck').addEventListener('click', function (evt) {
+	var y = evt.target;
+	if (y.nodeName === 'I') {
+		y = y.parentElement;
+	}
+	if ((y.nodeName === 'LI')&&(y.className === 'card')) {
+		showCard(y);
+		openCards.push(y);
+		if (openCards.length == 2) {
+			compareCards();
+		}
+	}
+});
+
+// Set up listener for restart button
+document.querySelector('.restart').addEventListener('click', paintCards);
 
 /*
  * Display the cards on the page
@@ -14,8 +45,6 @@ paintCards();
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
- 
 function paintCards() {
 	let randomCards = shuffle(orderedCards);
 	let htmlString = "";
@@ -26,7 +55,32 @@ function paintCards() {
 	openCards = [];
 	matchedPairs = 0;
 	playedTurns = 0;
-	document.querySelector('.moves').innerText = 0;
+	paintMoves();
+	paintStars();
+	runTimer();
+}
+
+// Display number of moves
+function paintMoves() {
+	document.querySelector('.moves').innerText = playedTurns;
+}
+
+// Display stars corresponding to number of moves
+function paintStars() {
+	let starHtmlString = "";
+	if (playedTurns > 16) {
+		gainedStars = 1;
+		starHtmlString = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star-o"></i></li><li><i class="fa fa-star-o"></i></li>';
+	}
+	else if (playedTurns > 8) {
+		gainedStars = 2;
+		starHtmlString = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star-o"></i></li>';
+	}
+	else {
+		gainedStars = 3;
+		starHtmlString = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+	}
+	document.querySelector('.stars').innerHTML = starHtmlString;
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -44,49 +98,31 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
- document.querySelector('.deck').addEventListener('click', function (evt) {
-	var y = evt.target;
-	if (y.nodeName === 'I') {
-		y = y.parentElement;
-	}
-	if ((y.nodeName === 'LI')&&(y.className === 'card')) {  
-		showCard(y);
-		openCards.push(y);
-		if (openCards.length == 2) {
-			compareCards();
-		}
-	}
-});
- 
- document.querySelector('.restart').addEventListener('click', paintCards);
- 
- function showCard(card) {
+// Display card sign
+function showCard(card) {
 	card.setAttribute('class', 'card open show');
- }
- 
- function hideCard(card) {
+}
+
+// Hide card sign
+function hideCard(card) {
 	card.setAttribute('class', 'card');
- }
- 
- function matchCard(card) {
+}
+
+// Display cards as matched
+function matchCard(card) {
 	card.setAttribute('class', 'card match');
- }
- 
- function compareCards() {
+}
+
+/* Compare cards
+ *  if they match, they'll display as so and stay open
+ *  if they do not match, they are removed from the list, the card's symbol is hidden
+ *  increment the move counter, display it on the page along with corresponding stars
+ *  if all cards have matched, display congrats message with the final score
+ */
+function compareCards() {
 	playedTurns++;
-	document.querySelector('.moves').innerText = playedTurns;
+	paintMoves();
+	paintStars();
 	let c1 = openCards.pop();
 	let c2 = openCards.pop();
 	setTimeout(function () {
@@ -102,11 +138,68 @@ function shuffle(array) {
 			hideCard(c1);
 			hideCard(c2);
 		}
-    }, 500);
+	}, 500);
 }
-	 
- function showWin() {
-	 //addStar();
-	 alert("DONE");
- }
-	 
+
+// Function showing win message, prompting to return to the game
+function showWin() {
+	document.querySelector('.container').style.display = "none";
+	document.querySelector('.win-cont').style.display = "block";
+	document.querySelector('.win-details').innerText = "With "+playedTurns+" moves and "+gainedStars+" stars! It took you only "+timeDetailedWin(playedSeconds)+"! Good job!!";
+	document.getElementById("play-again").onclick = function() {
+		document.querySelector('.win-cont').style.display = "none";
+		document.querySelector('.container').style.display = "flex";
+		paintCards();
+	}
+}
+
+// Starts timer. When there is a previous timer, stops it first
+function runTimer() {
+	if (timerId != null) {
+		clearInterval(timerId);
+	}
+	let startTime = Date.now();
+	timerId = setInterval(function() {
+		playedSeconds = Math.floor((Date.now() - startTime)/1000);
+		document.querySelector('.timer').innerText = timeDetailed(playedSeconds);
+	}, 1000);
+}
+
+// Represents timer as h,min,sec
+function timeDetailed(seconds) {
+	let minutes = 0;
+	let hours = 0;
+	if (seconds > 60) {
+		minutes = Math.floor(seconds/60);
+		seconds = seconds - minutes*60;
+		if (minutes > 60) {
+			hours = Math.floor(minutes/60);
+			minutes = minutes - hours*60;
+		}
+	}
+	let timeString = hours + " h " + minutes + " m " + seconds + " s";
+	return timeString;
+}
+
+//Represents time elapsed in format suitable for win message
+function timeDetailedWin(seconds) {
+	let timeString = "";
+	let minutes = 0;
+	let hours = 0;
+	if (seconds > 60) {
+		minutes = Math.floor(seconds/60);
+		seconds = seconds - minutes*60;
+		if (minutes > 60) {
+			hours = Math.floor(minutes/60);
+			minutes = minutes - hours*60;
+		}
+	}
+	if (hours > 0) {
+		timeString += hours + " h " + minutes + " min ";
+	}
+	else if (minutes > 0) {
+		timeString += minutes + " min ";
+	}
+	timeString += seconds + " sec";
+	return timeString;
+}
